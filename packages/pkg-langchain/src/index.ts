81e6a06ec592cc935a18190dcd3037a43bffdde6
han <han@fbrns.co>
2025-02-14 13:42:47 +0700

    feat(wip): add langchain

---
+++ packages/pkg-langchain/src/index.ts
@@ -0,0 +1,17 @@
+import { ChatGoogleGenerativeAI } from "@langchain/google-genai";
+
+const llm = new ChatGoogleGenerativeAI({
+  model: "gemini-1.5-pro",
+  temperature: 0,
+  maxRetries: 2,
+});
+
+const aiMsg = await llm.invoke([
+  [
+    "system",
+    "You are a helpful assistant that translates English to French. Translate the user sentence.",
+  ],
+  ["human", "I love programming."],
+]);
+
+console.dir(aiMsg, { depth: null });
